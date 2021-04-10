870c75eeb5d3e3a280885d3d516bddbbf0255b63
semper-7 <semper-7@yandex.ru>
2021-04-10 09:25:45 +0700

    Initial commit

---
+++ get.sh
@@ -0,0 +1,5 @@
+echo -e "GET / HTTP/1.1\r\nHost: bash.im\r\n\r\n" | nc bash.im 80
+
+$ nc www.rssweather.com 80
+GET /wx/in/kanpur/wx.php HTTP/1.0
+Host: www.rssweather.com
---
+++ microcore.cmd
@@ -0,0 +1,3 @@
+colinux-daemon.exe --install-driver
+colinux-daemon.exe -t nt @microcore.conf
+colinux-daemon.exe --remove-driver
